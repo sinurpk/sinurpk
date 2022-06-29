0556c6b0fdc94b5941ce2610e9a6f28940ee37e7
sinurpk <99707376+sinurpk@users.noreply.github.com>
2022-06-29 21:55:56 +0530

    Add files via upload

---
+++ addFn.py
@@ -0,0 +1,6 @@
+def add(a,b):
+   return a+b
+a=int(input('Enter number 1:'))
+b=int(input('Enter number 2:'))
+s=add(a,b)
+print("sum is ",s)
---
+++ mult2.py
@@ -0,0 +1,8 @@
+for n in range(1,6,+1):
+    for i in range(1,10,+1):
+        s=i*n
+        print(i,'*',n,'=',s)
+    
+    
+    
+
